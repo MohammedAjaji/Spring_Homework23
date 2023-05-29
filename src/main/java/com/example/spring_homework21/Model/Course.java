10e1876f99d5762d3842c1dc38a326ae6a781caf
Mohammed <malajaji.97@gmail.com>
2023-05-29 21:57:30 +0300

    All Good ^_^

--- src/main/java/com/example/spring_homework21/Model/Course.java
+++ src/main/java/com/example/spring_homework21/Model/Course.java
@@ -19,7 +19,6 @@ public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @NotEmpty(message = "name cannot be empty")
     @Column(columnDefinition = "varchar(20) not null")
     private String name;
